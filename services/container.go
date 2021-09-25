ad813fd4d029b229e64e2350a832957485a30ec0
往事随风 <gujiwork@outlook.com>
2021-09-26 00:07:52 +0800

    add: k8s cluster manage

--- services/container.go
+++ services/container.go
@@ -1,11 +1,19 @@
 package services
 
 import (
+	"fmt"
 	"pigs/common"
 	"pigs/models"
 )
 
 func CreateK8SCluster(cluster models.K8SCluster) (err error) {
 	err = common.GVA_DB.Create(&cluster).Error
+	return
+}
+
+func ListK8SCluster(cluster *[]models.K8SCluster) (err error) {
+
+	result := common.GVA_DB.Find(&cluster)
+	fmt.Printf("条数:%d", result.RowsAffected)
 	return
 }
