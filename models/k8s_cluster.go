d0d3a010403766131860d4a0e5da340a159cc84d
gujiwork <gujiwork@outlook.com>
2021-09-26 20:49:45 +0800

    feat：k8s集群 分页

--- models/k8s_cluster.go
+++ models/k8s_cluster.go
@@ -2,7 +2,7 @@ package models
 
 type K8SCluster struct {
 	ID             uint   `json:"id" gorm:"primarykey;AUTO_INCREMENT" form:"id"`
-	ClusterName    string `json:"clusterName" gorm:"comment:集群名称" binding:"required"`
+	ClusterName    string `json:"clusterName" gorm:"comment:集群名称" form:"clusterName" binding:"required"`
 	KubeConfig     string `json:"kubeConfig" gorm:"comment:集群凭证;type:varchar(12800)" binding:"required"`
 	ClusterVersion string `json:"clusterVersion" gorm:"comment:集群版本" binding:"required"`
 	NodeNumber     int8   `json:"nodeNumber" gorm:"comment:节点数"`
@@ -11,4 +11,11 @@ type K8SCluster struct {
 func (ks K8SCluster) TableName() string {
 	var k GModel
 	return k.TableName("k8s_cluster")
+}
+
+type PaginationQ struct {
+	Size    int    `form:"size" json:"size"`
+	Page    int    `form:"page" json:"page"`
+	Total   int64  `json:"total"`
+	Keyword string `form:"keyword" json:"keyword"`
 }
--- pigs_fe/src/api/user.js
+++ pigs_fe/src/api/user.js
@@ -1,4 +1,4 @@
 import {post} from "@/plugin/utils/request";
 
-export const login = (params) => post('/user/login', params)
+export const login = (params) => post('/api/v1/user/login', params)
 
