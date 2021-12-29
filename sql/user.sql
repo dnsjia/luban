f3801ece5dc4dab7ae4283d1b76bd269ac744dbb
micheng <safemonitor@outlook.com>
2021-12-29 11:43:42 +0800

    系统初始化SQL

--- sql/user.sql
+++
@@ -1,3 +0,0 @@
---- 用户luban@qq.com  密码test1234
-
-INSERT INTO `go_test`.`users` (`id`, `created_at`, `updated_at`, `deleted_at`, `username`, `password`, `phone`, `email`, `nick_name`, `avatar`, `status`, `role_id`, `dept_id`, `uid`, `create_by`) VALUES ('1', '2021-09-18 12:30:28', '2021-09-18 12:30:28', NULL, 'luban', '$2a$10$zn0dAfLmdIC2ff9dkAN7LezbwUFaHfsR6Aqis/jLHTJFy5rBUeey6', '0', 'luban@qq.com', '', 'http://qmplusimg.henrongyi.top/head.png', '1', '1', '0', NULL, NULL);
